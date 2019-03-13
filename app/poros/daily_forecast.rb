d5db823cf25330a82bc3ac3c678385ac5c6f1f95
Jimmy Smith <jaystansmith@gmail.com>
2019-03-12 18:47:47 -0600

    Added day attribute to daily forecast poro

--- app/poros/daily_forecast.rb
+++ app/poros/daily_forecast.rb
@@ -1,6 +1,7 @@
 class DailyForecast
-  attr_reader :icon, :precip, :high, :low
+  attr_reader :day, :icon, :precip, :high, :low
   def initialize(data)
+    @day =   Time.at(data[:time]).strftime("%A")
     @icon =   data[:icon]
     @precip = data[:precipProbability]
     @high =   data[:temperatureHigh]
